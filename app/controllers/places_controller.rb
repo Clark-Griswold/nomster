2d472a98a98fb2ba54e65b576aa733e6bddf82de
Clark-Griswold <dustindavis1@gmail.com>
2019-10-09 18:42:04 +0000

    tried to install will_paginate-bootstrap

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all.paginate(page: params[:page], per_page: 10)
+    @places = Place.paginate(page: params[:page], per_page: 5)
       #binding.pry
   end
 

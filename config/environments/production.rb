55deaf7995f82c0c72ea1808438d79c1d48d1ab5
Clark-Griswold <dustindavis1@gmail.com>
2019-10-11 02:09:33 +0000

    added devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = {host: 'nomster-project-two.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,4 +92,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+end
 end

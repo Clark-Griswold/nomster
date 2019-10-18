dd23bb8950065d7716e7d8359e1fcf2008bbea0d
Clark-Griswold <dustindavis1@gmail.com>
2019-10-18 02:41:33 +0000

    lesson 35

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,16 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
 
+  RATINGS = {
+    'one star': '1-star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
+  }
+
+
+  def humanize_rating
+    RATINGS.invert[self.rating]
+  end
 end

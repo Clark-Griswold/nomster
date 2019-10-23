e9a8dfa90a19c5ac6b07601e6605e81dfe960948
Clark-Griswold <dustindavis1@gmail.com>
2019-10-23 00:04:24 +0000

    few changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
-  config.fog_public = false
+
+  config.fog_public = true
   config.fog_credentials = {
     provider:             'AWS',
+    region: 'us-east',
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],
   }

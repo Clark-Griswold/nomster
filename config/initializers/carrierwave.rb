775c8952e3f4f052158e1e78f53a16827e3cbfe1
Clark-Griswold <dustindavis1@gmail.com>
2019-10-23 01:59:50 +0000

    AWS was wrong region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   config.fog_public = true
   config.fog_credentials = {
     provider:             'AWS',
-    region: 'us-east',
+    region: 'us-east-1',
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],
   }
